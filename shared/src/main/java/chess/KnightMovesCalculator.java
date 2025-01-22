2bf8e57c3960bd95a014e2d7d791fcc4d0504961
abbymuir47 <amuir4@byu.edu>
2025-01-22 12:40:23 -0700

    KnightMovesCalculator works

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -10,6 +10,40 @@ public class KnightMovesCalculator implements PieceMovesCalculator{
         Collection<ChessMove> moves = new ArrayList<>();
         int row = position.getRow();
         int col = position.getColumn();
+
+        ChessPiece myPiece = board.getPiece(position);
+        ChessGame.TeamColor myColor = myPiece.getTeamColor();
+
+        int[][] combinations = {
+                {1,2},
+                {1,-2},
+                {-1,2},
+                {-1,-2},
+                {2,1},
+                {2,-1},
+                {-2,1},
+                {-2,-1}
+        };
+
+        for (int i=0; i<combinations.length; i++){
+            int newRow = row + combinations[i][0];
+            int newCol = col + combinations[i][1];
+            ChessPosition newPos = new ChessPosition(newRow, newCol);
+            ChessMove currMove = new ChessMove(position, newPos, null);
+
+            if(newRow>=1 && newRow<=8 && newCol>=1 && newCol<=8){
+                if(board.getPiece(newPos) == null){
+                    moves.add(currMove);
+                }
+                else{
+                    ChessPiece newPiece = board.getPiece(newPos);
+                    ChessGame.TeamColor newPieceColor = newPiece.getTeamColor();
+                    if(newPieceColor != myColor){
+                        moves.add(currMove);
+                    }
+                }
+            }
+        }
         return moves;
     }
 }
