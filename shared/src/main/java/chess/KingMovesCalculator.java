2cb6994699d32d9279c5196afa8cb62204af055d
abbymuir47 <amuir4@byu.edu>
2025-01-21 20:35:40 -0700

    got KingMovesCalculator working

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -3,7 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import static chess.ChessGame.TeamColor.BLACK;
+import static chess.ChessGame.TeamColor.*;
 import static chess.ChessPiece.PieceType.KING;
 import static chess.ChessPiece.PieceType.PAWN;
 
@@ -15,31 +15,41 @@ public class KingMovesCalculator implements PieceMovesCalculator{
         int row = position.getRow();
         int col = position.getColumn();
 
-        /*
-        [0][1]
-        [0][-1]
-        [1][0]
-        [-1][0]
-        [1][1]
-        [1][-1]
-        [-1][1]
-        [-1][-1]
-        */
-
         //defines my current piece and gets its color
         ChessPiece myPiece = board.getPiece(position);
         ChessGame.TeamColor pieceColor = myPiece.getTeamColor();
 
-        ChessPosition endPosition;
-        if(pieceColor == BLACK){
-            endPosition = new ChessPosition(row-1,col);
-        } else { //if the color is white
-            endPosition = new ChessPosition(row+1,col);
-        }
+        int[][] combinations = {
+                {0,1},
+                {0,-1},
+                {1,0},
+                {-1,0},
+                {1,1},
+                {1,-1},
+                {-1,1},
+                {-1,-1}
+        };
 
-        ChessPiece checkPiece = board.getPiece(endPosition);
-        if(checkPiece != null){
-            moves.add(new ChessMove(position, endPosition,KING));
+        for (int i = 0; i < combinations.length; i++) {
+            int newRow = row + combinations[i][0];
+            int newCol = col + combinations[i][1];
+            ChessPosition currMove = new ChessPosition(newRow, newCol);
+
+            if(newRow<1 | newRow>8 | newCol<1 | newCol>8){
+                continue;
+            }
+            else{
+                ChessPiece newPos = board.getPiece(currMove);
+                if(newPos != null){
+                    ChessGame.TeamColor newSquareColor = newPos.getTeamColor();
+                    if(newSquareColor != pieceColor){
+                        moves.add(new ChessMove(position, currMove, null));
+                    }
+                }
+                else{
+                    moves.add(new ChessMove(position, currMove, null));
+                }
+            }
         }
         return moves;
     }
