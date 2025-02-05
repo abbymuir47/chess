c7ecb8b2c4440a4ccb77445d3155afcce9717a3b
abbymuir47 <amuir4@byu.edu>
2025-02-04 23:59:44 -0700

    realized BlackCheck() wasn't working bc it counted the rook's moves without regard to piecetype - reverted to old code and added a check for if the position putting something in danger was the right piece type. still not working tho

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,7 +3,7 @@ package chess;
 import java.util.Collection;
 import java.util.Objects;
 
-import static chess.ChessPiece.PieceType.KING;
+import static chess.ChessPiece.PieceType.*;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -57,8 +57,8 @@ public class ChessGame {
      * If there is no piece at that location, this method returns null.
      * A move is valid if it is a "piece move" for the piece at the input location and
      * making that move would not leave the team’s king in danger of check.
-    */
-     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
+     */
+    public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         throw new RuntimeException("Not implemented");
         /*
         if(board.getPiece(startPosition)==null){
@@ -95,184 +95,220 @@ public class ChessGame {
         int row = kingPosition.getRow();
         int col = kingPosition.getColumn();
 
-        if (checkJumpAttack(teamColor, row, col)){
+        if (checkKnightAttack(teamColor, row, col)){
             return true;
         }
-        if (checkRecursiveAttack(teamColor, row, col)){
+        if (checkKingAttack(teamColor, row, col)){
             return true;
         }
-        if(teamColor == TeamColor.WHITE){
-            int newRow = row + 1;
-            return checkPawnAttack(teamColor, newRow, col);
+        if (checkRookAttack(teamColor, row, col)){
+            return true;
         }
-        else if(teamColor == TeamColor.BLACK){
-            int newRow = row - 1;
-            return checkPawnAttack(teamColor, newRow, col);
+        if (checkBishopAttack(teamColor, row, col)){
+            return true;
         }
-        else{
-            return false;
-        }
-    }
-
-    private boolean checkPawnAttack(TeamColor teamColor, int newRow, int col) {
-        for(int i=-1; i<2; i+=2){
-            if(col+i >0 && col+i <9){
-                ChessPosition attack = new ChessPosition(newRow, col +i);
-                ChessPiece attackPiece = board.getPiece(attack);
-                if(attackPiece != null){
-                    TeamColor newPieceColor = attackPiece.getTeamColor();
-                    if(newPieceColor != teamColor){
-                        return true;
-                    }
-                }
-            }
+        if (checkQueenAttack(teamColor, row, col)){
+            return true;
         }
         return false;
+
+        //throw new RuntimeException("Not implemented");
     }
 
-    private boolean checkJumpAttack(TeamColor teamColor, int row, int col) {
+    private boolean checkKnightAttack(TeamColor teamColor, int row, int col){
         int[][] combinations = {
-                //possible opposing knight positions
-                {1,2}, {1,-2}, {-1,2}, {-1,-2}, {2,1}, {2,-1}, {-2,1}, {-2,-1},
-                //possible opposing king positions
-                {0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}
+                {1,2},
+                {1,-2},
+                {-1,2},
+                {-1,-2},
+                {2,1},
+                {2,-1},
+                {-2,1},
+                {-2,-1}
         };
-        for (int i = 0; i < combinations.length; i++) {
-            int newRow = row + combinations[i][0];
-            int newCol = col + combinations[i][1];
-            ChessPosition newPos = new ChessPosition(newRow, newCol);
-            ChessPiece newPiece;
+        return checkJumpAttack(teamColor, combinations, row, col, KNIGHT);
+    }
 
-            if(newRow<1 | newRow>8 | newCol<1 | newCol>8){
-                continue;
-            }
-            else{
-                newPiece = board.getPiece(newPos);
-                if(newPiece != null){
-                    TeamColor newPieceColor = newPiece.getTeamColor();
-                    if(newPieceColor != teamColor){
-                        return true;
-                    }
-                }
-                else{
+    private boolean checkKingAttack(TeamColor teamColor, int row, int col){
+        int[][] combinations = {
+                {0,1},
+                {0,-1},
+                {1,0},
+                {-1,0},
+                {1,1},
+                {1,-1},
+                {-1,1},
+                {-1,-1}
+        };
+        return checkJumpAttack(teamColor, combinations, row, col, KING);
+    }
+    private boolean checkRookAttack(TeamColor teamColor, int row, int col){
+        int[][] combinations = {
+                {1,0},
+                {-1,0},
+                {0,1},
+                {0,-1}
+        };
+        return checkRecursiveAttack(teamColor, combinations, row, col, ROOK);
+    }
+
+    private boolean checkBishopAttack(TeamColor teamColor, int row, int col){
+        int[][] combinations = {
+                {1,1},
+                {1,-1},
+                {-1,1},
+                {-1,-1}
+        };
+        return checkRecursiveAttack(teamColor, combinations, row, col, BISHOP);
+    }
+
+        private boolean checkQueenAttack(TeamColor teamColor, int row, int col){
+            int[][] combinations = {
+                    {1,0},
+                    {-1,0},
+                    {0,1},
+                    {0,-1},
+                    {1,1},
+                    {1,-1},
+                    {-1,1},
+                    {-1,-1}
+            };
+            return checkRecursiveAttack(teamColor, combinations, row, col, QUEEN);
+        }
+
+        private boolean checkJumpAttack(TeamColor teamColor, int[][] combinations, int row, int col, ChessPiece.PieceType type) {
+            for (int i = 0; i < combinations.length; i++) {
+                int newRow = row + combinations[i][0];
+                int newCol = col + combinations[i][1];
+                ChessPosition newPos = new ChessPosition(newRow, newCol);
+                ChessPiece newPiece;
+
+                if(newRow<1 | newRow>8 | newCol<1 | newCol>8){
                     continue;
                 }
-            }
-        }
-        return false;
-    }
-
-    private boolean checkRecursiveAttack(TeamColor teamColor, int row, int col){
-        int[][] combinations = {
-                //possible opposing rook positions
-                {1,0}, {-1,0}, {0,1}, {0,-1},
-                //possible opposing bishop positions
-                {1,1}, {1,-1}, {-1,1}, {-1,-1}
-        };
-
-        for (int i=0; i<combinations.length; i++){
-            int newRow = row + combinations[i][0];
-            int newCol = col + combinations[i][1];
-
-            while(newRow>=1 && newRow<=8 && newCol>=1 && newCol<=8){
-                ChessPosition newPos = new ChessPosition(newRow, newCol);
-                if(board.getPiece(newPos) != null){
-                    ChessPiece newPiece = board.getPiece(newPos);
-                    ChessGame.TeamColor newPieceColor = newPiece.getTeamColor();
-                    if(newPieceColor != teamColor){
-                        return true;
+                else{
+                    newPiece = board.getPiece(newPos);
+                    if(newPiece != null){
+                        TeamColor newPieceColor = newPiece.getTeamColor();
+                        if(newPieceColor != teamColor){
+                            if(newPiece.getPieceType() == type){
+                                return true;
+                            }
+                        }
                     }
                     else{
-                        break;
-                    }
-                }
-                else{
-                    newRow += combinations[i][0];
-                    newCol += combinations[i][1];
-                }
-            }
-        }
-        return false;
-    }
-
-    private ChessPosition getKingPosition(TeamColor teamColor) {
-        ChessPosition currPos;
-        ChessPiece currPiece;
-        for(int i = 1; i<9; i++){
-            for(int j=1;j<9;j++){
-                currPos = new ChessPosition(i,j);
-                currPiece = board.getPiece(currPos);
-                if (currPiece != null && currPiece.getTeamColor() == teamColor){
-                    if(currPiece.getPieceType() == KING){
-                        return currPos;
+                        continue;
                     }
                 }
             }
-        }
-        return null;
-    }
-
-    /**
-     * Determines if the given team is in checkmate
-     *
-     * @param teamColor which team to check for checkmate
-     * @return True if the specified team is in checkmate
-     * Returns true if the given team has no way to protect their king from being captured
-     */
-    public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
-    }
-
-    /**
-     * Determines if the given team is in stalemate, which here is defined as having
-     * no valid moves
-     *
-     * @param teamColor which team to check for stalemate
-     * @return True if the specified team is in stalemate, otherwise false
-     * Returns true if the given team has no legal moves but their king is not in immediate danger.
-     * can't make any moves that wouldn't put your king in check
-     */
-    public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
-    }
-
-    /**
-     * Sets this game's chessboard with a given board
-     *
-     * @param board the new board to use
-     */
-    public void setBoard(ChessBoard board) {
-        this.board = board;
-    }
-
-    /**
-     * Gets the current chessboard
-     *
-     * @return the chessboard
-     */
-    public ChessBoard getBoard() {
-        return board;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        ChessGame chessGame = (ChessGame) o;
-        return Objects.equals(board, chessGame.board) && team == chessGame.team;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(board, team);
-    }
+        private boolean checkRecursiveAttack(TeamColor teamColor, int[][] combinations, int row, int col, ChessPiece.PieceType type){
+            for (int i=0; i<combinations.length; i++){
+                int newRow = row + combinations[i][0];
+                int newCol = col + combinations[i][1];
+                while(newRow>=1 && newRow<=8 && newCol>=1 && newCol<=8){
+                    ChessPosition newPos = new ChessPosition(newRow, newCol);
+                    if(board.getPiece(newPos) != null){
+                        ChessPiece newPiece = board.getPiece(newPos);
+                        ChessGame.TeamColor newPieceColor = newPiece.getTeamColor();
+                        if(newPieceColor != teamColor){
+                            if(newPiece.getPieceType() == type){
+                                return true;
+                            }
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                    else{
+                        newRow += combinations[i][0];
+                        newCol += combinations[i][1];
+                    }
+                }
+            }
+            return false;
+        }
 
-    @Override
-    public String toString() {
-        return "ChessGame{" +
-                "board=" + board +
-                ", team=" + team +
-                '}';
+        private ChessPosition getKingPosition(TeamColor teamColor) {
+            ChessPosition currPos;
+            ChessPiece currPiece;
+            for(int i = 1; i<9; i++){
+                for(int j=1;j<9;j++){
+                    currPos = new ChessPosition(i,j);
+                    currPiece = board.getPiece(currPos);
+                    if (currPiece != null && currPiece.getTeamColor() == teamColor){
+                        if(currPiece.getPieceType() == KING){
+                            return currPos;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        /**
+         * Determines if the given team is in checkmate
+         *
+         * @param teamColor which team to check for checkmate
+         * @return True if the specified team is in checkmate
+         * Returns true if the given team has no way to protect their king from being captured
+         */
+        public boolean isInCheckmate(TeamColor teamColor) {
+            throw new RuntimeException("Not implemented");
+        }
+
+        /**
+         * Determines if the given team is in stalemate, which here is defined as having
+         * no valid moves
+         *
+         * @param teamColor which team to check for stalemate
+         * @return True if the specified team is in stalemate, otherwise false
+         * Returns true if the given team has no legal moves but their king is not in immediate danger.
+         * can't make any moves that wouldn't put your king in check
+         */
+        public boolean isInStalemate(TeamColor teamColor) {
+            throw new RuntimeException("Not implemented");
+        }
+
+        /**
+         * Sets this game's chessboard with a given board
+         *
+         * @param board the new board to use
+         */
+        public void setBoard(ChessBoard board) {
+            this.board = board;
+        }
+
+        /**
+         * Gets the current chessboard
+         *
+         * @return the chessboard
+         */
+        public ChessBoard getBoard() {
+            return board;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ChessGame chessGame = (ChessGame) o;
+            return Objects.equals(board, chessGame.board) && team == chessGame.team;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(board, team);
+        }
+
+        @Override
+        public String toString() {
+            return "ChessGame{" +
+                    "board=" + board +
+                    ", team=" + team +
+                    '}';
+        }
     }
-}
