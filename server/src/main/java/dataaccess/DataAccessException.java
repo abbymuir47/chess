c911844b9669abcf74bad52c0c2fab980e20327e
abbymuir47 <amuir4@byu.edu>
2025-03-06 23:52:49 -0700

    added the starter code for phase 4, need to add the dependencies now

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -6,6 +6,11 @@ package dataaccess;
 public class DataAccessException extends Exception{
     final private int statusCode;
 
+    public DataAccessException(String message) {
+        super(message);
+        this.statusCode = -1;
+    }
+
     public DataAccessException(int statusCode, String message) {
         super(message);
         this.statusCode = statusCode;
