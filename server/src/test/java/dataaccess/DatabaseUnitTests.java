3eaa0581d6ee376cc73e8acdd8e5140fb09c782e
abbymuir47 <amuir4@byu.edu>
2025-03-12 15:34:57 -0600

    now, createUser, getUser, and clearUserDAO are all working and passing the tests I wrote.

--- server/src/test/java/dataaccess/DatabaseUnitTests.java
+++ server/src/test/java/dataaccess/DatabaseUnitTests.java
@@ -23,10 +23,23 @@ public class DatabaseUnitTests {
     }
 
     @Test
-    public void createUserSuccess() throws SQLException{
-        UserData newUser = new UserData("myUser", "myPassword", "myEmail");
-        System.out.println("create:" + sqlUserDataAccess.createUser(newUser));
-        System.out.println("get:" + sqlUserDataAccess.getUser("myUser"));
+    public void createUserSuccess() throws SQLException {
+        UserData newUser = new UserData("user1", "password1", "email1");
+
+        UserData isCreated = sqlUserDataAccess.createUser(newUser);
+        System.out.println("User created: " + isCreated);
+
+        UserData retrievedUser = sqlUserDataAccess.getUser("user1");
+        System.out.println("Retrieved user: " + retrievedUser);
+
+        Assertions.assertNotNull("User should be created", String.valueOf(isCreated)); // Checks if the user was created
+        Assertions.assertNotNull("User should be retrieved", String.valueOf(retrievedUser)); // Checks if user was found
+        Assertions.assertEquals("user1", "user1", retrievedUser.username()); // Ensures the username matches
+    }
+
+    @Test
+    public void clearUserSuccess() throws SQLException, DataAccessException {
+        sqlUserDataAccess.clearUserDAO();
     }
 
 }
