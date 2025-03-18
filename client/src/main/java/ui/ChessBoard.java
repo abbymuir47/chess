b89f40b5dd9908842dbc26c58fbca7d648904af1
abbymuir47 <amuir4@byu.edu>
2025-03-17 18:58:25 -0600

    got alternating black and white squares of the chessboard

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -27,13 +27,11 @@ public class ChessBoard {
         out.print(ERASE_SCREEN);
 
         drawHeaders(out);
-        drawTicTacToeBoard(out);
+        drawChessBoard(out);
     }
 
     private static void drawHeaders(PrintStream out) {
-
-        setBlack(out);
-
+        out.print(SET_BG_COLOR_LIGHT_GREY);
         String[] headers = { "a","b","c","d","e","f","g","h" };
         out.print(" ");
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
@@ -43,7 +41,6 @@ public class ChessBoard {
                 out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
             }
         }
-
         out.println();
     }
 
@@ -57,7 +54,7 @@ public class ChessBoard {
     }
 
     private static void printHeaderText(PrintStream out, String letter) {
-        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_BG_COLOR_LIGHT_GREY);;
         out.print(SET_TEXT_COLOR_GREEN);
 
         out.print(letter);
@@ -65,63 +62,40 @@ public class ChessBoard {
         setBlack(out);
     }
 
-    private static void drawTicTacToeBoard(PrintStream out) {
+    private static void drawChessBoard(PrintStream out) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
-
-            drawRowOfSquares(out);
-
-//            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
-//                // Draw horizontal row separator.
-//                drawHorizontalLine(out);
-//                setBlack(out);
-//            }
-        }
-    }
-
-    private static void drawRowOfSquares(PrintStream out) {
-
-        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
-            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-                setWhite(out);
-
-                if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
-                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
-
-                    out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
-                    out.print(EMPTY.repeat(suffixLength));
+                if (boardRow % 2 == 0) {
+                    drawWhiteRowOfSquares(out);
                 }
                 else {
-                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
+                    drawBlackRowOfSquares(out);
                 }
-
-//                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-//                    // Draw vertical column separator.
-//                    setRed(out);
-//                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
-//                }
-
-                setBlack(out);
-            }
-
-            out.println();
         }
     }
 
-    private static void drawHorizontalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
-            setRed(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
-            out.println();
+    private static void drawWhiteRowOfSquares(PrintStream out) {
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            if (boardCol % 2 == 0){
+                drawWhiteSquare(out, rand.nextBoolean() ? X : O);
+            }else{
+                drawBlackSquare(out, rand.nextBoolean() ? X : O);
+            }
         }
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.println();
+    }
+
+    private static void drawBlackRowOfSquares(PrintStream out) {
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            if (boardCol % 2 == 0){
+                drawBlackSquare(out, rand.nextBoolean() ? X : O);
+            }else{
+                drawWhiteSquare(out, rand.nextBoolean() ? X : O);
+            }
+        }
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.println();
     }
 
     private static void setWhite(PrintStream out) {
@@ -129,22 +103,20 @@ public class ChessBoard {
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void setRed(PrintStream out) {
-        out.print(SET_BG_COLOR_RED);
-        out.print(SET_TEXT_COLOR_RED);
-    }
-
     private static void setBlack(PrintStream out) {
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
-    private static void printPlayer(PrintStream out, String player) {
+    private static void drawWhiteSquare(PrintStream out, String player) {
         out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
-
+        out.print(SET_TEXT_COLOR_MAGENTA);
         out.print(player);
+    }
 
-        setWhite(out);
+    private static void drawBlackSquare(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_MAGENTA);
+        out.print(player);
     }
 }
