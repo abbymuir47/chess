26fab1f89147a813d7463b5dfb59b7ac12ae95c1
abbymuir47 <amuir4@byu.edu>
2025-03-24 22:39:57 -0600

    reverted port back to the right one, but the unit tests aren't passing. need to check tomorrow

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -20,7 +20,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(80);
+        var port = server.run(0);
         String url = ("http://localhost:" + port + "/");
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(url);
