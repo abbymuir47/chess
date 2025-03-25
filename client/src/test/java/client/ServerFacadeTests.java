5a9eb70367e537f679c8e015848da415a8841cc2
abbymuir47 <amuir4@byu.edu>
2025-03-24 22:36:23 -0600

    finished writing unit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -5,6 +5,7 @@ import exception.ResponseException;
 import handlermodel.RegisterRequest;
 import handlermodel.*;
 import org.junit.jupiter.api.*;
+import org.opentest4j.AssertionFailedError;
 import server.Server;
 import server.ServerFacade;
 
@@ -19,7 +20,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(80);
         String url = ("http://localhost:" + port + "/");
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(url);
@@ -46,43 +47,85 @@ public class ServerFacadeTests {
     public void registerFail() throws ResponseException {
         RegisterRequest req2 = new RegisterRequest("player2", "password", "p2@email.com");
         facade.register(req2);
-        RegisterRequest req3 = new RegisterRequest("player2", "password", "p3@email.com");
-        assertThrows(ResponseException.class, ()-> facade.register(req3));
+        RegisterRequest repeatReq = new RegisterRequest("player2", "password", "p3@email.com");
+        assertThrows(ResponseException.class, ()-> facade.register(repeatReq));
     }
 
     @Test
     public void loginSuccess() throws ResponseException {
+        RegisterRequest req3 = new RegisterRequest("player3", "password", "p3@email.com");
         LoginRequest req = new LoginRequest("player1", "password");
         LoginResult res = facade.login(req);
         assertTrue(res.authToken().length() > 10);
     }
 
     @Test
-    public void loginFail(){
+    public void loginFail() throws ResponseException{
+        RegisterRequest req4 = new RegisterRequest("player4", "password", "p3@email.com");
+        facade.register(req4);
+        LoginRequest req = new LoginRequest("player4", "wrongPassword");
+        assertThrows(ResponseException.class, ()-> facade.login(req));
     }
 
     @Test
-    public void createGameSuccess(){
+    public void createGameSuccess() throws ResponseException {
+        RegisterRequest req5 = new RegisterRequest("player5", "password", "p5@email.com");
+        facade.register(req5);
+        LoginRequest req = new LoginRequest("player5", "password");
+        facade.login(req);
+
+        CreateRequest gameReq = new CreateRequest("game2");
+        CreateResult res = facade.createGame(gameReq);
+        assertTrue(res.gameID() > 0);
     }
 
     @Test
-    public void createGameFail(){
+    public void createGameFail() throws ResponseException {
+        RegisterRequest req6 = new RegisterRequest("player6", "password", "p6@email.com");
+        facade.register(req6);
+        LoginRequest req = new LoginRequest("player6", "password");
+        facade.login(req);
+
+        CreateRequest nullReq = new CreateRequest(null);
+        assertThrows(ResponseException.class, ()-> facade.createGame(nullReq));
     }
 
     @Test
-    public void listGamesSuccess(){
+    public void listGamesSuccess() throws ResponseException {
+        RegisterRequest req7 = new RegisterRequest("player7", "password", "p7@email.com");
+        facade.register(req7);
+        LoginRequest req = new LoginRequest("player7", "password");
+        facade.login(req);
+
+        ListResult res = facade.listGames();
+        assertTrue(res.games() != null);
     }
 
     @Test
-    public void listGamesFail(){
+    public void listGamesFail() throws ResponseException {
+        assertThrows(ResponseException.class, ()-> facade.listGames());
     }
 
     @Test
-    public void joinGameSuccess(){
+    public void joinGameSuccess() throws ResponseException {
+        RegisterRequest req8 = new RegisterRequest("player8", "password", "p8@email.com");
+        facade.register(req8);
+        LoginRequest req = new LoginRequest("player8", "password");
+        facade.login(req);
+
+        JoinRequest joinRequest = new JoinRequest("BLACK", 1);
+        assertNotNull(facade.joinGame(joinRequest));
     }
 
     @Test
-    public void joinGameFail(){
+    public void joinGameFail() throws ResponseException {
+        RegisterRequest req9 = new RegisterRequest("player9", "password", "p9@email.com");
+        facade.register(req9);
+        LoginRequest req = new LoginRequest("player9", "password");
+        facade.login(req);
+
+        JoinRequest joinRequest = new JoinRequest("GREEN", 1);
+        assertThrows(ResponseException.class, ()-> facade.joinGame(joinRequest));
     }
 
 }
