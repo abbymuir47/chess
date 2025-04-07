6e22b5b4f181ef77bad33c1cbf99384d4e1c65d2
abbymuir47 <amuir4@byu.edu>
2025-04-07 12:02:30 -0600

    added another websocket file

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,32 @@
+package server.websocket;
+
+public class WebSocketHandler {
+}
+
+/*
+@OnWebSocketMessage
+public void onMessage(Session session, String msg) {
+  try {
+    UserGameCommand command = Serializer.fromJson(message, UserGameCommand.class);
+
+    // Throws a custom UnauthorizedException. Yours may work differently.
+    String username = getUsername(command.getAuthString());
+
+    saveSession(command.getGameID(), session);
+
+    switch (command.getCommandType()) {
+      case CONNECT -> connect(session, username, (ConnectCommand) command);
+      case MAKE_MOVE -> makeMove(session, username, (MakeMoveCommand) command);
+      case LEAVE -> leaveGame(session, username, (LeaveGameCommand) command);
+      case RESIGN -> resign(session, username, (ResignCommand) command);
+    }
+  } catch (UnauthorizedException ex) {
+    // Serializes and sends the error message
+    sendMessage(session.getRemote(), new ErrorMessage("Error: unauthorized"));
+  } catch (Exception ex) {
+    ex.printStackTrace();
+    sendMessage(session.getRemote(), new ErrorMessage("Error: " + ex.getMessage()));
+  }
+}
+
+ */
